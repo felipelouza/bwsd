6b7290764c08a2d1be86cd39c3302be9d181dcb8
Simon Gog <simon.gog@gmail.com>
2018-05-19 11:42:20 -0700

    added distance computation

--- bwsd_rmq.cpp
+++ bwsd_rmq.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <string>
+#include <map>
 #include <sdsl/rmq_support.hpp>
 
 using namespace std;
 using namespace sdsl;
+
+typedef unordered_map<uint64_t,uint64_t> tUMII;
+
+double compute_distance(const tUMII& m, size_t runs) {
+    double dist = 0;
+    for(const auto& it : m) {
+        double tmp = (double)it.second / (double) runs;
+        dist += tmp*log2(tmp);
+    }
+    dist *= -1.0;
+    return dist;
+}
 
 int main(){
     vector<uint64_t> D = {0,1,2,3,2,2,1,2,2,3,4,2,1,2,3,4,2,1,2,1,0,5,4,3,2,3,2,1,3,2,3,4,5,3,2,3,3,1,1,1,2,0};
@@ -54,10 +67,13 @@ int main(){
     rmq_succinct_sct<true>  rmq_P(&P);
     rmq_succinct_sct<false> RMQ_N(&P);
 
-    vector<uint64_t> seen(maxD+1,0); 
-    stack<uint64_t>  seen_stack;
-    vector<uint64_t> freq(maxD+1,0); 
-    vector<uint64_t> last_occ(maxD+1, 0);
+    vector<size_t> seen(maxD+1,0); 
+    stack<size_t>  seen_stack;
+    vector<size_t> freq(maxD+1,0); 
+    vector<size_t> last_occ(maxD+1, 0);
+
+    vector<vector<tUMII>> counts(maxD+1, vector<tUMII>(maxD+1));
+    vector<vector<uint64_t>> runs(maxD+1, vector<uint64_t>(maxD+1));
 
     for(size_t i=1; i < D.size() + maxD; ++i){
         size_t d  = i < D.size() ? D[i] : i-D.size();
@@ -79,6 +95,18 @@ int main(){
             auto x = seen_stack.top();
             seen_stack.pop();
             cout << " (d="<<x<<", f="<<freq[x]<<")";
+            auto i1 = std::min(d,x);
+            auto i2 = std::max(d,x);
+            ++counts[i1][i2][freq[x]];
+            ++runs[i1][i2];
+        }
+        cout << endl;
+    }
+
+    for(size_t i=0; i<=maxD; ++i){
+        for(size_t j=i+1; j<=maxD; ++j) {
+            double dist = compute_distance(counts[i][j], runs[i][j]);
+            cout << dist << " "; 
         }
         cout << endl;
     }
